e730f58043d656c350520121c7ea7d3633e0994a
Ruslan Sabirov <sabirov@worksolusions.ru>
2014-12-30 13:45:06 +0300

    Модуль ws-tools. Добавлен вывод описания модуля в Настройках модулей

--- include.php
+++ include.php
@@ -1,4 +1,8 @@
 <?php
 /**
  * @author Sabirov Ruslan <sabirov@worksolutions.ru>
- */ 
+ */
+require_once __DIR__ . '/src/module.php';
+require_once __DIR__ . '/src/localization.php';
+require_once __DIR__ . '/src/module.php';
+require_once __DIR__ . '/src/options.php';
---
+++ lang/ru/info.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'name' => 'Инструменты WS',
+    'description' => 'Позволяет производить конвертацию свойтсв Инфо Блоков',
+    'partner' => array(
+        'name' => 'Рабочие Решения',
+        'url' => 'http://www.worksolutions.ru'
+    )
+);
