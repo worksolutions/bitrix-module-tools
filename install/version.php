8d59675a2380bab3a2baed1189ab27d24ce2364e
Max Sokolovsky <sokolovsky@worksolutions.ru>
2015-05-13 12:55:36 +0300

    Повышение версии. Удаление мусора через updater

--- install/version.php
+++ install/version.php
@@ -6,6 +6,6 @@
 $fileName = __DIR__.'/version.php';
 
 $arModuleVersion = array(
-    'VERSION' => '1.2.11',
+    'VERSION' => '1.2.12',
     'VERSION_DATE' => '2015-05-15'
 );
--- updater.php
+++ updater.php
@@ -12,3 +12,10 @@ $fAddErrorMessage = function ($mess) use ($updater){
     $updater->errorMessage[] = $mess;
 };
 //=====================================================
+
+if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+    $root = $_SERVER['DOCUMENT_ROOT'];
+    unlink($root.'/bitrix/modules/ws.tools/lib/mail/MailPackage.php');
+    unlink($root.'/bitrix/modules/ws.tools/lib/mail/MailService.php');
+    unlink($root.'/bitrix/modules/ws.tools/lib/tests/cases/EventManagerTestCase.php');
+}
