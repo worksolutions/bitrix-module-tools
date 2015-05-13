9541ac7f90cac590e1a2c7f37a2c53324aaedcee
Max Sokolovsky <sokolovsky@worksolutions.ru>
2015-05-13 11:50:01 +0300

    Повышение версии связанное с добавлением автотестов.

--- install/version.php
+++ install/version.php
@@ -6,6 +6,6 @@
 $fileName = __DIR__.'/version.php';
 
 $arModuleVersion = array(
-    'VERSION' => '1.2.10',
-    'VERSION_DATE' => '2015-04-25'
+    'VERSION' => '1.2.11',
+    'VERSION_DATE' => '2015-05-15'
 );
