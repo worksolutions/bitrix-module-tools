faaf1c32152afe8622748d3d5753f7a660867e75
Smotrov Dmitriy <dsxack@gmail.com>
2015-07-09 13:57:32 +0300

    Релиз 1.2.15: очистка updater.php

--- updater.php
+++ updater.php
@@ -12,8 +12,3 @@ $fAddErrorMessage = function ($mess) use ($updater){
     $updater->errorMessage[] = $mess;
 };
 //=====================================================
-
-$root = $_SERVER['DOCUMENT_ROOT'];
-
-DeleteDirFilesEx($root.'/bitrix/modules/ws.tools/.git');
-unlink($root.'/bitrix/modules/ws.tools/.gitignore');
